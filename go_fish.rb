9a790ffaef80d67c8df2a2f8c275f926989e2f6d
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-10-05 16:24:00 -0400

    Each player has a hand now and I can compare hands to user input

--- go_fish.rb
+++ go_fish.rb
@@ -9,11 +9,18 @@ class Card
   def output_card
     puts "The #{@rank} of #{@suit}"
   end
+
+  def output_rank
+    puts "#{@rank}"
+  end
 end
 
 class Deck
+  attr_accessor :cards, :ranks
+
+
   def initialize
-    @ranks = [*(2..10), 'J', 'Q', 'K', 'A']
+    @ranks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", 'J', 'Q', 'K', 'A']
     @suits = ['S', 'H', 'D', 'C']
     @cards = []
 
@@ -23,33 +30,55 @@ class Deck
       end
     end
 
-  @cards.shuffle!
+    @cards.shuffle!
   end
 
-  def deal (number)
+  def deal(number)
+    da_hand = []
+    number.times do
+      tmpcard = @cards.pop
+      tmpcard.output_card
+      da_hand << tmpcard
+    end
+    da_hand
+  end
+  def deal_p2(number)
     number.times {@cards.shift.output_card}
   end
 end
 
+class Player
+  attr_accessor :hand
 
+  def initialize
+    @hand = []
+  end
+end
+
+def cls
+  system('cls')
+end
+
+def the_check(p1hand, p2hand, deck)
+  puts "What Rank do you want to check for?"
+  input = $stdin.gets.chomp
+  p2hand.each do |card|
+    if card.rank == input
+      puts "you got one"
+      return true
+    end
+  end
+  return false
+end
 
 deck = Deck.new
-deck.deal(7)
+player1 = Player.new
+player2 = Player.new
+p1hand = player1.hand
+p2hand = player2.hand
+p1hand = deck.deal(7)
+puts "\n \n"
+p2hand = deck.deal(7)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
+the_check(p1hand, p2hand, deck)
